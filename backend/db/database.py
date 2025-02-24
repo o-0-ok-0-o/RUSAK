7117b90732d0597267a4bbe7830389783494a55d
Pavlov D <danpavkzn@mail.ru>
2025-02-24 10:53:08 +0300

    структура бэкенд проекта

---
+++ backend/db/database.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from fastapi import Depends
+
+# from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+# from sqlalchemy.orm import DeclarativeBase
+# from src.core.config import settings
+# from sqlalchemy.ext.asyncio import AsyncSession
+
+
+# class Base(DeclarativeBase):
+#     pass
+#
+#
+# async_engine = create_async_engine(url=settings.DATABASE_URL_asyncpg)
+# async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
+#
+#
+# async def get_async_session():
+#     async with async_session_factory() as session:
+#         yield session
