6e7c0c2e57b647f9e8302e960837e379a96f776f
Pavlov Danil <danpavkzn@mail.ru>
2025-02-26 22:43:56 +0300

    почистил ненужный конфиг

--- backend/config.py
+++ backend/config.py
@@ -1,32 +1,22 @@
 from pathlib import Path
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-class AuthJWT(BaseModel):
-    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
-    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
-    algorithm: str = "RS256"
-    access_token_expired_minutes: int = 15
-    refresh_token_expired_days: int = 30
+# class Settings(BaseSettings):
+    # DB_HOST: str
+    # DB_PORT: int
+    # DB_USER: str
+    # DB_PASS: str
+    # DB_NAME: str
+
+    # @property
+    # def DATABASE_URL_asyncpg(self):
+    #     # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
+    #     return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    # model_config = SettingsConfigDict(env_file=".env")
 
 
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    @property
-    def DATABASE_URL_asyncpg(self):
-        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    auth_jwt: AuthJWT = AuthJWT()
-    model_config = SettingsConfigDict(env_file=".env")
-
-
-settings = Settings()
+# settings = Settings()
