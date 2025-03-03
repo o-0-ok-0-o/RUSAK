1ab4a0979daffbdcf30fb9b267f84125f8b30d01
o-0-ok-0-o <azattnurr79@gmail.com>
2025-03-03 10:31:06 +0300

    Delete backend directory

--- backend/utils/datetime.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pytz
-
-
-def get_current_moscow_time() -> str:
-    """Возвращает текущую дату и время в московском часовом поясе в формате YYYY-MM-DD HH:MM:SS."""
-    moscow_tz = pytz.timezone("Europe/Moscow")  # Московский часовой пояс
-    moscow_time = datetime.now(moscow_tz)  # Текущее время в Москве
-    return moscow_time.strftime("%d-%m-%Y %H:%M")
