9356015a31fd1cda34d238a4209dfa6b2b4f8397
Pavlov D <danpavkzn@mail.ru>
2025-03-06 00:50:01 +0300

    сделаны обращения к модели engine = работае доставание всех двигателей + видно к каким машинам они привязаны

---
+++ backend/api_v1/crud/create_engine.py
@@ -0,0 +1,54 @@
+from fastapi import Depends, Query, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload
+from typing import Annotated
+from api_v1.schemas.schemas import EngineBase
+from db.database import get_async_session
+from db.models import Engine, Car
+
+
+async def create_engine(
+    engine: Annotated[EngineBase, Depends()],
+    session: AsyncSession = Depends(get_async_session),
+):
+    engine_dict: dict = engine.model_dump()
+    engine_model = Engine(**engine_dict)
+    session.add(engine_model)
+    await session.commit()
+    return engine
+
+
+async def get_all_engines(
+    session: AsyncSession,  # = Depends(get_async_session),
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    engines = await session.execute(
+        select(Engine).options(selectinload(Engine.cars)).offset(offset).limit(limit),
+    )
+    return engines.scalars().all()
+
+
+async def get_engine(
+    engine_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    engine = await session.get(Engine, engine_id)
+
+    if not engine:
+        raise HTTPException(status_code=404, detail="Двигатель не найден")
+    return engine
+
+
+async def delete_engine(
+    engine_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    engine = await session.get(Engine, engine_id)
+
+    if engine is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    await session.delete(engine)
+    await session.commit()
+    return {"success": True}
--- backend/api_v1/routers/all.py
+++ backend/api_v1/routers/all.py
@@ -1,42 +1,58 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.database import get_async_session
+
+from api_v1.crud.create_engine import get_all_engines
+from api_v1.schemas.schemas import (
+    CarRead,
+    EngineRead,
+    SaloneMemberRead,
+    SaloneOptionRead,
+    ServiceRead,
+    ShassiRead,
+    ZipRead,
+)
 
 router = APIRouter(
-    prefix="/orders",
-    tags=["Заказы"],
+    prefix="/test-crud",
+    tags=["Тестовые"],
 )
 
 
-@app.get("/car", response_model=list[CarSchema])
-def get_extras():
-
-    return available_extras
-
-
-@app.get("/engine", response_model=list[Option])
-def get_models():
-    return available_models
+@router.get("/engine", response_model=list[EngineRead])
+async def get_engines(
+    session: AsyncSession = Depends(get_async_session),
+):
+    engines = await get_all_engines(session)
+    return engines
 
 
-@app.get("/salone-member", response_model=list[Option])
+@router.get("/salone-member", response_model=list[SaloneMemberRead])
 def get_colors():
-    return available_colors
+    return 1
 
 
-@app.get("/salone-option", response_model=list[Option])
+@router.get("/salone-option", response_model=list[SaloneOptionRead])
 def get_engines():
-    return available_engines
+    return 1
 
 
-@app.get("/service", response_model=list[Option])
+@router.get("/service", response_model=list[ServiceRead])
 def get_extras():
-    return available_extras
+    return 1
 
 
-@app.get("/shassi", response_model=list[Option])
+@router.get("/shassi", response_model=list[ShassiRead])
 def get_extras():
-    return available_extras
+    return 1
 
 
-@app.get("/zip", response_model=list[Option])
+@router.get("/zip", response_model=list[ZipRead])
 def get_extras():
-    return available_extras
+    return 1
+
+
+@router.get("/car", response_model=list[CarRead])
+def get_extras():
+    return 1
