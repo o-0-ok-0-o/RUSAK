8d66906cba72afcea3fb3cb9c425ec84965c18db
Pavlov D <danpavkzn@mail.ru>
2025-03-05 22:34:36 +0300

    порядок = разбиение на модули

---
+++ backend/api_v1/crud/create_objects.py
@@ -0,0 +1,108 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import Car, Engine, SaloneMember, SaloneOption, Service, Shassi, Zip
+
+
+async def create_car(
+    car_name: str,
+    base_price: int,
+    engine_id: int,
+    salone_member_id: int,
+    session: AsyncSession,
+) -> Car:
+    car = Car(
+        car_name=car_name,
+        base_price=base_price,
+        engine_id=engine_id,
+        salone_member_id=salone_member_id,
+    )
+    session.add(car)
+    await session.commit()
+    return car
+
+
+async def create_engine(
+    engine_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> Engine:
+    engine = Engine(
+        engine_name=engine_name,
+        base_price=base_price,
+    )
+    session.add(engine)
+    await session.commit()
+    return engine
+
+
+async def create_salonemember(
+    salone_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> SaloneMember:
+    salone_member = SaloneMember(
+        salone_name=salone_name,
+        base_price=base_price,
+    )
+
+    session.add(salone_member)
+    await session.commit()
+    return salone_member
+
+
+async def create_saloneoption(
+    salone_option_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> SaloneOption:
+    salone_option = SaloneOption(
+        salone_option_name=salone_option_name,
+        base_price=base_price,
+    )
+
+    session.add(salone_option)
+    await session.commit()
+    return salone_option
+
+
+async def create_service(
+    service_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> Service:
+    service = Service(
+        service_name=service_name,
+        base_price=base_price,
+    )
+
+    session.add(service)
+    await session.commit()
+    return service
+
+
+async def create_shassi(
+    shassi_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> Shassi:
+    shassi = Shassi(
+        shassi_name=shassi_name,
+        base_price=base_price,
+    )
+    session.add(shassi)
+    await session.commit()
+    return shassi
+
+
+async def create_zip(
+    zip_name: str,
+    base_price: int,
+    session: AsyncSession,
+) -> Zip:
+    zip1 = Zip(
+        zip_name=zip_name,
+        base_price=base_price,
+    )
+    session.add(zip1)
+    await session.commit()
+    return zip1
--- backend/api_v1/crud/demo_auto_crud.py
+++ backend/api_v1/crud/demo_auto_crud.py
@@ -2,116 +2,20 @@ import asyncio
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload, joinedload
-from db.database import async_session_factory, drop_tables, create_tables
-from db.models import Engine, SaloneMember, Car, Service, SaloneOption, Shassi, Zip
+
+from api_v1.crud.create_objects import (
+    create_car,
+    create_engine,
+    create_salonemember,
+    create_saloneoption,
+    create_service,
+    create_shassi,
+    create_zip,
+)
+from db.database import async_session_factory
+from db.models import Car, Service, SaloneOption, Shassi, Zip
 
 
-async def create_car(
-    car_name: str,
-    base_price: int,
-    engine_id: int,
-    salone_member_id: int,
-    session: AsyncSession,
-) -> Car:
-    car = Car(
-        car_name=car_name,
-        base_price=base_price,
-        engine_id=engine_id,
-        salone_member_id=salone_member_id,
-    )
-    session.add(car)
-    await session.commit()
-    return car
-
-
-async def create_engine(
-    engine_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> Engine:
-    engine = Engine(
-        engine_name=engine_name,
-        base_price=base_price,
-    )
-    session.add(engine)
-    await session.commit()
-    return engine
-
-
-async def create_salonemember(
-    salone_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> SaloneMember:
-    salone_member = SaloneMember(
-        salone_name=salone_name,
-        base_price=base_price,
-    )
-
-    session.add(salone_member)
-    await session.commit()
-    return salone_member
-
-
-async def create_saloneoption(
-    salone_option_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> SaloneOption:
-    salone_option = SaloneOption(
-        salone_option_name=salone_option_name,
-        base_price=base_price,
-    )
-
-    session.add(salone_option)
-    await session.commit()
-    return salone_option
-
-
-async def create_service(
-    service_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> Service:
-    service = Service(
-        service_name=service_name,
-        base_price=base_price,
-    )
-
-    session.add(service)
-    await session.commit()
-    return service
-
-
-async def create_shassi(
-    shassi_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> Shassi:
-    shassi = Shassi(
-        shassi_name=shassi_name,
-        base_price=base_price,
-    )
-    session.add(shassi)
-    await session.commit()
-    return shassi
-
-
-async def create_zip(
-    zip_name: str,
-    base_price: int,
-    session: AsyncSession,
-) -> Zip:
-    zip1 = Zip(
-        zip_name=zip_name,
-        base_price=base_price,
-    )
-    session.add(zip1)
-    await session.commit()
-    return zip1
-
-
-################################
 async def create_cars_and_relations(
     session: AsyncSession,
 ):
@@ -235,9 +139,11 @@ async def demo_m2m(session: AsyncSession):
 async def main():
     # await drop_tables()
     # await create_tables()
-    async with async_session_factory() as session:
-        # await create_cars_and_relations(session)
-        await demo_m2m(session)
+    # async with async_session_factory() as session:
+    #     await create_cars_and_relations(session)
+    #     await demo_m2m(session)
+    pass
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
