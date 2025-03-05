f2aa68775c4969467f2c8432c09526692abc67ff
Pavlov D <danpavkzn@mail.ru>
2025-03-05 23:04:15 +0300

    присоединен тестовый роутер

--- backend/api_v1/routers/__init__.py
+++ backend/api_v1/routers/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from api_v1.routers.orders import router as order_router
+from api_v1.routers.all import router as all_router
 
 router = APIRouter(
     prefix="/api_v1/v1",
 )
 
 router.include_router(order_router)
+router.include_router(all_router)
